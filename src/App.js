7add7db93619f75c174cd4c58d6cccb298e8e82b
B Severino <biancasev@gmail.com>
2020-02-26 23:36:26 -0500

    Fixed file structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
-import Registration from './user-management/Registration'
+import Registration from './components/user/Registration'
 
 const App = () => {
   return (
     <div>
       <h1>Secret Family Recipes Cookbook</h1>
-      <Route path="/registration" exact component={Registration} />
+      <Route path='/registration' exact component={Registration} />
     </div>
   )
 }
