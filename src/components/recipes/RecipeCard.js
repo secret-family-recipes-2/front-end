00b97d314192f3f120710399857380fa9bf05396
B Severino <biancasev@gmail.com>
2020-02-26 22:19:20 -0500

    Built recipe list component and added pills to recipe cards

--- src/components/recipes/RecipeCard.js
+++ src/components/recipes/RecipeCard.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import {
-  Card,
+  Card as ReactCard,
   CardImg,
   CardText,
   CardBody,
   CardTitle,
   CardSubtitle,
   Button,
+  Badge,
 } from 'reactstrap'
+
+const Card = styled(ReactCard)`
+  width: 318px;
+  margin-bottom: 50px;
+`
 
 const RecipeCard = props => {
   return (
@@ -21,7 +29,17 @@ const RecipeCard = props => {
       <CardBody>
         <CardTitle>Name</CardTitle>
         <CardSubtitle>Source</CardSubtitle>
-        <CardText>Description</CardText>
+        <Badge color='dark' pill>
+          Category 1
+        </Badge>
+        <Badge color='dark' pill>
+          Category 2
+        </Badge>
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et
+          lobortis elit. Integer vel urna sed libero vulputate ornare in ut
+          odio.
+        </CardText>
         <Button>View Recipe</Button>
       </CardBody>
     </Card>
