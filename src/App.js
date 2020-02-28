97aa44974185eef6d4baa35b14ead4f2418ef8a5
B Severino <biancasev@gmail.com>
2020-02-27 22:38:14 -0500

    Router updated with all current components and private routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Switch, Route, Link, Redirect } from 'react-router-dom'
+
+import ProtectedRoute from './utils/ProtectedRoute'
 
 import Registration from './components/user/Registration'
 import LoginForm from './components/user/LoginForm'
 import AddNewRecipe from './components/recipes/AddRecipe'
+import RecipeList from './components/recipes/RecipeList'
 
 const App = () => {
   return (
     <div>
-      <nav className="nav-links">
-      <h2>Secret Family Recipes Cookbook</h2>
-      <div>
-      
-      <Link to="/login">LogIn</Link>
-      <Link to="/registration">SignUp</Link>
-      </div>
+      <nav className='nav-links'>
+        <h2>Secret Family Recipes Cookbook</h2>
+        <div>
+          <Link to='/login'>Log In</Link>
+          <Link to='/registration'>Sign Up</Link>
+        </div>
       </nav>
-      
-      <Route path='/registration' exact component={Registration} />
-      <Route path='/login' component ={LoginForm} />
-      <AddNewRecipe/> 
+
+      {/* Switch just checks each path in order down the list */}
+      <Switch>
+        {/* Protected routes check for a token, and redirect to the login if there is none */}
+        <ProtectedRoute path='/addrecipe' component={AddNewRecipe} />
+        <ProtectedRoute path='/recipes' component={RecipeList} />
+        <Route path='/registration' component={Registration} />
+        <Route path='/login' component={LoginForm} />
+
+        {/* If user has a token, redirect to recipe list, else redirect to login */}
+        <Route path='/'>
+          {localStorage.getItem('token') ? (
+            <Redirect to='/recipes' />
+          ) : (
+            <Redirect to='/login' />
+          )}
+        </Route>
+      </Switch>
     </div>
   )
 }
