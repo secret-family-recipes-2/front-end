e034f4bc0a2e34351a425f45e106116c7235f0a5
askaborg <hienle@askaborg.com>
2020-02-28 14:27:13 -0800

    refactor links to Navigation.js

---
+++ src/components/Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
+
+const Navigation = () => {
+    const history = useHistory()
+
+    const logout = e => {
+        e.preventDefault()
+        localStorage.removeItem('token')
+        history.push('/login')
+    }
+
+    return (
+        <nav className='nav-links'>
+            <h2>Secret Family Recipes Cookbook</h2>
+            <div>
+                <Link to='/recipes'>View Recipes</Link>
+                <Link to='/addrecipe'>Add Recipe</Link>
+                <Link to='/login'>Log In</Link>
+                <Link to='/registration'>Sign Up</Link>
+                <Link to='#' onClick={logout}>
+                    Log Out
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation
+
