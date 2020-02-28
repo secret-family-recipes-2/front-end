98114870bbd246b98e5d19d922690aea6adb336f
askaborg <hienle@askaborg.com>
2020-02-27 20:17:59 -0800

    unposted commented

--- src/components/user/RegistrationForm.js
+++ src/components/user/RegistrationForm.js
@@ -58,3 +58,71 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps, { postRegister })(RegistrationForm)
+
+// import React, { useState, useEffect } from 'react'
+// import { withFormik, Form, Field } from 'formik'
+// import * as Yup from 'yup'
+// import axios from 'axios'
+
+// const DisplayForm = ({ values, errors, touched, status }) => {
+//     const [userList, addUser] = useState([])
+
+//     useEffect(() => status && addUser(userList => [...userList, status]), [status])
+
+//     return (
+//         <div className="registration-form">
+//             <Form>
+//                 <div>
+//                     {touched.name && errors.name && (<p>{errors.name}</p>)}   
+//                     <Field type="text" name="name" placeholder="Name" />
+//                 </div>
+//                 <div>
+//                     {touched.email && errors.email && (<p>{errors.email}</p>)}
+//                     <Field type="text" name="email" placeholder="Email" />
+//                 </div>
+//                 <div>
+//                     {touched.password && errors.password && (<p>{errors.password}</p>)}    
+//                     <Field type="password" name="password" placeholder="Passcode" />
+//                 </div>
+//                 <div>
+//                     <br></br>
+//                     <input type="submit" value="Submit" /> 
+//                 </div>
+//             </Form>   
+//         </div>
+//     )
+// }
+
+// const RegistrationForm = withFormik({
+//     mapPropsToValues({ name, email, password }) {
+//         return {
+//             name: name || "",
+//             email: email || "",
+//             password: password || ""
+//         }
+//     },
+//     validationSchema: Yup.object().shape({
+//         name: Yup.string()
+//             .min(2, "Name requires at least 2 characters")
+//             .max(50, "Name cannot be more than 50 characters")
+//             .required("You need a name"),
+//         email: Yup.string()
+//             .email("You need a real email")
+//             .required("You need an email"),
+//         password: Yup.string()
+//             .min(6, "Passcode needs to be at lease 6 characters")
+//             .required("You need a passcode")
+//     }),
+//     handleSubmit(values, { setStatus, resetForm }){
+//         axios
+//             .post("https://secret-recipes-2.herokuapp.com/api", values)
+//             .then(responseHerokuapp => {
+//                 console.log('response', responseHerokuapp.data)
+//                 setStatus(responseHerokuapp.data)
+//             })
+//             .catch(errorHerokuapp => console.log(errorHerokuapp.response))
+//             .finally(resetForm())
+//     }
+// })(DisplayForm)
+
+// export default RegistrationForm
