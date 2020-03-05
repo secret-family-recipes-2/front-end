753e7917f8608f8474410c1ad6cd261177893565
B Severino <biancasev@gmail.com>
2020-03-04 22:11:45 -0500

    Added background and loading spinners, styled registration to match login

--- src/components/recipes/RecipeList.js
+++ src/components/recipes/RecipeList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Container, Row, Col } from 'reactstrap'
-import { useSelector } from 'react-redux'
+import { Container, Row, Col, Card, CardBody } from 'reactstrap'
+import { SpinnerDiv, Spinner } from '../styled-components/spinner'
 import { axiosWithAuth } from '../../utils/axiosWithAuth'
 
 import RecipeCard from './RecipeCard'
@@ -10,6 +10,7 @@ const RecipeList = () => {
   const userId = Number(localStorage.getItem('userId'))
   const [allRecipes, setAllRecipes] = useState([])
   const [err, setErr] = useState([])
+  const [isFetching, setIsFetching] = useState(true)
 
   console.log(userId)
 
@@ -18,10 +19,14 @@ const RecipeList = () => {
       .get('https://secret-recipes-2.herokuapp.com/api/recipes/allRecipes')
       .then(res => {
         setAllRecipes(res.data.filter(recipe => recipe.user_id === userId))
+        setIsFetching(false)
         console.log('allRecipes', res)
       })
 
-      .catch(err => setErr(err))
+      .catch(err => {
+        setErr(err)
+        setIsFetching(false)
+      })
   }, [userId])
 
   const gotallRecipes = allRecipes.length !== 0 ? true : false
@@ -29,9 +34,15 @@ const RecipeList = () => {
 
   console.log(gotallRecipes)
 
-  if (gotallRecipes)
+  if (isFetching)
     return (
-      <Container>
+      <SpinnerDiv>
+        <Spinner color='success' />
+      </SpinnerDiv>
+    )
+  else if (gotallRecipes)
+    return (
+      <Container style={{ margin: '50px auto' }}>
         <Row>
           {allRecipes.map(recipe => (
             <Col xs='12' sm='6' md='4' key={recipe.id}>
@@ -47,8 +58,13 @@ const RecipeList = () => {
         No recipes yet? <Link to='/addrecipe'>Add one!</Link>
       </div>
     )
-  else if (gotError) return <p>{err.message}</p>
-  else return <div>Loading...</div>
+  else if (gotError)
+    return (
+      <Card>
+        <CardBody>{err.message}</CardBody>
+      </Card>
+    )
+  else return <></>
 }
 
 export default RecipeList
